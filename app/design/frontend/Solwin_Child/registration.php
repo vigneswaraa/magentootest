92a5904553cd71d2b14ddfbcb6fafc91079654d6
niveditha <niveditha.prevaj@gmail.com>
2019-03-15 13:28:27 +0530

    firstcommit

---
+++ app/design/frontend/Solwin_Child/registration.php
@@ -0,0 +1,10 @@
+<?php
+ 
+\Magento\Framework\Component\ComponentRegistrar::register(
+ 
+  \Magento\Framework\Component\ComponentRegistrar::THEME,
+ 
+  'frontend/Solwin/Solwin_child',
+ 
+  __DIR__
+);
